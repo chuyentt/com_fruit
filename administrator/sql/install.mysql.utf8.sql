d38808a3c32c60cc6dca23cf3697d60e47386be4
ducsatthu <ductranxuan.29710@gmail.com>
2015-04-29 23:13:13 +0700

    fix not insert
    fix not insert

--- administrator/sql/install.mysql.utf8.sql
+++ administrator/sql/install.mysql.utf8.sql
@@ -16,6 +16,6 @@ CREATE TABLE IF NOT EXISTS `#__fruit_oranges` (
 PRIMARY KEY (`id`)
 ) DEFAULT COLLATE=utf8_general_ci;
 
-# Add new rows to content_types table for version history.
+/** Add new rows to content_types table for version history. **/
 INSERT INTO `#__content_types` (`type_id`, `type_title`, `type_alias`, `table`, `rules`, `field_mappings`, `router`, `content_history_options`) VALUES
 (null, 'Orange', 'com_fruit.orange', '{"special":{"dbtable":"#__fruit_orange","key":"id","type":"Orange","prefix":"FruitTable","config":"array()"},"common":{"dbtable":"#__ucm_content","key":"ucm_id","type":"Corecontent","prefix":"JTable","config":"array()"}}', '', '{"common":{"core_content_item_id":"id","core_title":"title","core_state":"published","core_alias":"alias","core_created_time":"created_time","core_modified_time":"modified_time","core_body":"description", "core_hits":"hits","core_publish_up":"null","core_publish_down":"null","core_access":"access", "core_params":"params", "core_featured":"featured", "core_metadata":"metadata", "core_language":"language", "core_images":"images", "core_urls":"urls", "core_version":"version", "core_ordering":"null", "core_metakey":"metakey", "core_metadesc":"metadesc", "core_catid":"null", "core_xreference":"null", "asset_id":"null"}, "special":{"field1":"field1"}}', 'EpsgHelperRoute::getProjectionRoute', '{"formFile":"administrator\/components\/com_fruit\/models\/forms\/orange.xml", "hideFields":["checked_out","checked_out_time"], "ignoreChanges":["checked_out", "checked_out_time"], "convertToInt":[], "displayLookup":[]}');
